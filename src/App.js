47a245112ca0d24fc3cf82732ec58484ce55a163
Emanuel <manupalacios19@gmail.com>
2021-03-12 16:08:12 -0300

    Add Books component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Books from './Books';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <NavLink to='/books'>Books</NavLink>
+            </div>
+            <Switch>
+                <Route path='/books' component={Books} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Books.js
@@ -0,0 +1,18 @@
+import React from "react";
+import axios from "axios";
+
+const Books = () => {
+    const [books, setBooks] = React.useState([]);
+    React.useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/books")
+            .then((response) => {
+                setBooks(response.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+    const bookList = books.map((book) => <li key={book.id}>{book.title}</li>);
+    return <ul>{bookList}</ul>;
+};
+
+export default Books;
